9d7e1dcd3b95d03e0f3ea58c883f74ce2fb47cbc
Jai <jaipanchal100@gmail.com>
2022-11-30 20:23:32 +0530

    sub folders

---
+++ rock_paper_scissor/src/main/java/com/rock_paper_scissor/RockPaperScissorApplication.java
@@ -0,0 +1,15 @@
+package com.rock_paper_scissor;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorApplication.class, args);
+		System.out.println("testing.....");
+	}
+
+}
